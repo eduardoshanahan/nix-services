f38c51bc5682f4a181c1a17916066f8c2a64c2fa
Eduardo Shanahan <email@eduardoshanahan.com>
2026-01-25 07:17:20 +0000

    removed pihole before update

--- flake.nix
+++ flake.nix
@@ -42,17 +42,14 @@
     // (
       let
         traefikModule = import ./services/traefik/traefik.nix;
-        piholeModule = import ./services/pihole/pihole.nix;
       in
       {
         nixosModules = {
           traefik = traefikModule;
-          pihole = piholeModule;
         };
 
         services = {
           traefik = traefikModule;
-          pihole = piholeModule;
         };
       }
     );
