1b2aa54d403597d202f2a97d0a21eb0900fc0508
Eduardo Shanahan <eduardo.shanahan@arrowheadmind.com>
2026-04-08 16:09:36 +0100

    tailscale: bump default image tag to v1.96.5

--- services/tailscale/tailscale.nix
+++ services/tailscale/tailscale.nix
@@ -122,7 +122,7 @@ in {
 
       tag = lib.mkOption {
         type = lib.types.str;
-        default = "v1.94.2";
+        default = "v1.96.5";
         description = "Container image tag.";
       };
 
